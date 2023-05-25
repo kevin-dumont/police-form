d56ee214c8d7bb025d18f43c859ac46a915da27a
Kévin Dumont <kevin_dumont.external@discovery.com>
2023-05-25 22:26:33 +0200

    ci: fix

--- packages/api/infrastructure/api-gateway/modules/lambda/api-gateway.tf
+++ packages/api/infrastructure/api-gateway/modules/lambda/api-gateway.tf
@@ -1,50 +1,50 @@
 
-# resource "aws_api_gateway_integration" "integration" {
-#   rest_api_id             = var.api_gateway_config.rest_api_id
-#   resource_id             = aws_api_gateway_resource.resource.id
-#   http_method             = aws_api_gateway_method.method.http_method
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = var.api_gateway_config.rest_api_id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
   
-#   type                    = "AWS_PROXY"
-#   integration_http_method = var.api_gateway_config.http_method
-#   uri                     = aws_lambda_function.lambda.arn
-# }
+  type                    = "AWS_PROXY"
+  integration_http_method = var.api_gateway_config.http_method
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
 
-# resource "aws_api_gateway_resource" "resource" {
-#   rest_api_id = var.api_gateway_config.rest_api_id
-#   parent_id   = var.api_gateway_config.parent_id
-#   path_part   = var.api_gateway_config.path_part
-# }
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = var.api_gateway_config.rest_api_id
+  parent_id   = var.api_gateway_config.parent_id
+  path_part   = var.api_gateway_config.path_part
+}
 
-# resource "aws_api_gateway_method" "method" {
-#   resource_id   = aws_api_gateway_resource.resource.id
-#   rest_api_id   = var.api_gateway_config.rest_api_id
-#   http_method   = var.api_gateway_config.http_method
-#   authorization = "NONE"
-# }
+resource "aws_api_gateway_method" "method" {
+  resource_id   = aws_api_gateway_resource.resource.id
+  rest_api_id   = var.api_gateway_config.rest_api_id
+  http_method   = var.api_gateway_config.http_method
+  authorization = "NONE"
+}
 
-# resource "aws_api_gateway_method_response" "cors_response" {
-#   rest_api_id = var.api_gateway_config.rest_api_id
-#   resource_id = aws_api_gateway_resource.resource.id
-#   http_method = var.api_gateway_config.http_method
-#   status_code = "200"
+resource "aws_api_gateway_method_response" "cors_response" {
+  rest_api_id = var.api_gateway_config.rest_api_id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = var.api_gateway_config.http_method
+  status_code = "200"
 
-#   response_parameters = {
-#     "method.response.header.Access-Control-Allow-Origin" = true,
-#     "method.response.header.Access-Control-Allow-Headers" = true,
-#     "method.response.header.Access-Control-Allow-Methods" = true,
-#   }
-# }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true,
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+  }
+}
 
-# resource "aws_api_gateway_integration_response" "cors_integration" {
-#   depends_on  = [aws_api_gateway_method_response.cors_response]
-#   rest_api_id = var.api_gateway_config.rest_api_id
-#   resource_id = aws_api_gateway_resource.resource.id
-#   http_method = var.api_gateway_config.http_method
-#   status_code = "200"
+resource "aws_api_gateway_integration_response" "cors_integration" {
+  depends_on  = [aws_api_gateway_method_response.cors_response]
+  rest_api_id = var.api_gateway_config.rest_api_id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = var.api_gateway_config.http_method
+  status_code = "200"
 
-#   response_parameters = {
-#     "method.response.header.Access-Control-Allow-Origin" = "\"${join(",", var.allowed_origins)}\"",
-#     "method.response.header.Access-Control-Allow-Headers" = "\"${join(",", var.allowed_headers)}\"",
-#     "method.response.header.Access-Control-Allow-Methods" = "\"${join(",", var.allowed_methods)}\""
-#   }
-# }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "\"${join(",", var.allowed_origins)}\"",
+    "method.response.header.Access-Control-Allow-Headers" = "\"${join(",", var.allowed_headers)}\"",
+    "method.response.header.Access-Control-Allow-Methods" = "\"${join(",", var.allowed_methods)}\""
+  }
+}
