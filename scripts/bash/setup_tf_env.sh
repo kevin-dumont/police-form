1857b878ae0359713c8d335bac216b5765cd3de9
Kévin Dumont <kevin_dumont.external@discovery.com>
2023-05-25 15:33:58 +0200

    ci: fix

--- scripts/bash/setup_tf_env.sh
+++ scripts/bash/setup_tf_env.sh
@@ -3,4 +3,3 @@ export $(grep -v '^#' .env | xargs)
 export TF_VAR_AWS_REGION=$AWS_REGION
 export TF_VAR_API_KEY=$API_KEY
 export TF_VAR_WEB_BUCKET_NAME=$WEB_BUCKET_NAME
-export TF_VAR_WEB_API_KEY=$WEB_API_KEY
