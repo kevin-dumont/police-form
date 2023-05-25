954bb553e2333fb37071693d5b8bdf0eb5b6d8c4
Kévin Dumont <kevin_dumont.external@discovery.com>
2023-05-25 16:49:31 +0200

    ci: fix

--- infrastructure/api-gateway.tf
+++ infrastructure/api-gateway.tf
@@ -14,21 +14,21 @@ resource "aws_api_gateway_resource" "resource" {
 }
 
 # API Gateway method
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_method" "method_post" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_method" "method_get" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_method" "method_delete" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "DELETE"
@@ -36,7 +36,7 @@ resource "aws_api_gateway_method" "method" {
 }
 
 
-resource "aws_api_gateway_method" "method" {
+resource "aws_api_gateway_method" "method_put" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "PUT"
