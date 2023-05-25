128553904d1e09cf6b374669670d2e193d1ad843
Kévin Dumont <kevin_dumont.external@discovery.com>
2023-05-25 22:15:46 +0200

    ci: fix

--- packages/api/infrastructure/api-gateway/modules/lambda/api-gateway.tf
+++ packages/api/infrastructure/api-gateway/modules/lambda/api-gateway.tf
@@ -1,26 +1,26 @@
 
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = var.api_gateway_config.rest_api_id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
+# resource "aws_api_gateway_integration" "integration" {
+#   rest_api_id             = var.api_gateway_config.rest_api_id
+#   resource_id             = aws_api_gateway_resource.resource.id
+#   http_method             = aws_api_gateway_method.method.http_method
   
-  type                    = "AWS_PROXY"
-  integration_http_method = var.api_gateway_config.http_method
-  uri                     = aws_lambda_function.lambda.arn
-}
+#   type                    = "AWS_PROXY"
+#   integration_http_method = var.api_gateway_config.http_method
+#   uri                     = aws_lambda_function.lambda.arn
+# }
 
-resource "aws_api_gateway_resource" "resource" {
-  rest_api_id = var.api_gateway_config.rest_api_id
-  parent_id   = var.api_gateway_config.parent_id
-  path_part   = var.api_gateway_config.path_part
-}
+# resource "aws_api_gateway_resource" "resource" {
+#   rest_api_id = var.api_gateway_config.rest_api_id
+#   parent_id   = var.api_gateway_config.parent_id
+#   path_part   = var.api_gateway_config.path_part
+# }
 
-resource "aws_api_gateway_method" "method" {
-  resource_id   = aws_api_gateway_resource.resource.id
-  rest_api_id   = var.api_gateway_config.rest_api_id
-  http_method   = var.api_gateway_config.http_method
-  authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "method" {
+#   resource_id   = aws_api_gateway_resource.resource.id
+#   rest_api_id   = var.api_gateway_config.rest_api_id
+#   http_method   = var.api_gateway_config.http_method
+#   authorization = "NONE"
+# }
 
 # resource "aws_api_gateway_method_response" "cors_response" {
 #   rest_api_id = var.api_gateway_config.rest_api_id
