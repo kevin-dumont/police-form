e7e22d35f84215c3fa47ef62c684a30e5892ef9e
Kévin Dumont <kevin_dumont.external@discovery.com>
2023-06-16 09:41:00 +0200

    fix: zip script

--- scripts/bash/api-zip.sh
+++ scripts/bash/api-zip.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-find ./packages/api -name build -type d | while read dir; do
-  parent_dir=$(dirname "$dir")
-  pkg_name=$(jq -r .name <"$parent_dir/package.json")
+# sauvegarder le répertoire de travail actuel
+root_dir=$(pwd)
+
+# créez le répertoire build s'il n'existe pas
+mkdir -p "$root_dir/infrastructure/$NODE_ENV/build"
+
+# Trouver tous les fichiers package.json dans ./packages/api
+find ./packages/api/resources -name package.json -type f | while read pkg; do
+  parent_dir=$(dirname "$pkg")
+  pkg_name=$(jq -r .name <"$pkg")
   pkg_name=${pkg_name/@/}
   pkg_name=${pkg_name/\//-}
 
-  # Check if the package name is not empty
-  if [ -z "$pkg_name" ]; then
-    echo "Package name is empty for directory $parent_dir, skipping."
-    continue
+  echo "> $pkg_name"
+
+  # cd dans le répertoire contenant le package.json
+  cd "$parent_dir"
+
+  # Vérifie si un répertoire de build et un fichier index.js existent avant de créer le zip
+  if [ -d "./build" ] && [ -f "./build/index.js" ]; then
+    cd ./build
+    zip -r "$root_dir/infrastructure/$NODE_ENV/build/$pkg_name.zip" "./index.js"
+  else
+    echo "No build directory or index.js file found in $parent_dir, skipping."
   fi
 
-  echo $pkg_name
-
-  zip -r "infrastructure/$NODE_ENV/build/$pkg_name.zip" "$dir"
+  # Retourne au répertoire racine
+  cd "$root_dir"
 done
