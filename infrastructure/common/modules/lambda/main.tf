f7b92e8d021ca41d398a8f45cfe6a7eb47d0745f
Kévin Dumont <kevin_dumont.external@discovery.com>
2023-06-16 10:35:48 +0200

    fix: tf

--- infrastructure/common/modules/lambda/main.tf
+++ infrastructure/common/modules/lambda/main.tf
@@ -1,11 +1,17 @@
+data "aws_s3_object" "object" {
+  bucket = "traveler-form-terraform"
+  key    = var.s3_path
+}
+
 resource "aws_lambda_function" "lambda" {
   function_name    = "${var.env}-${var.function_name}"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "build/index.handler"
   runtime          = "nodejs18.x"
 
-  s3_bucket        = "traveler-form-terraform"
-  s3_key           = var.s3_path
+  s3_bucket        = data.aws_s3_object.object.bucket
+  s3_key           = data.aws_s3_object.object.key
+  source_code_hash = data.aws_s3_object.object.etag
 
   tags = {
     Name        = "Lambda"
